42f6165433753bea9772d34828510b95b242265b
vuthysok90@gmail.com <Vuthysok627>
2024-11-24 21:07:32 -1000

    home

---
+++ projects/finalproject/finaljavascript.js
@@ -0,0 +1,40 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    
+    if (!firstname || !lastname) {
+        alert("First name and Last name are required!");
+        return;
+    }
+    if (!age || age < 18) {
+        alert("You must be 18 to sign up!");
+        return;
+    }
+
+    
+    const Data = {
+        firstname: firstname,
+        lastname: lastname,
+        age: age,
+    };
+    
+    const xhr = new XMLHttpRequest(); 
+    xhr.open("GET", "b.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText); 
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    
+    xhr.send(JSON.stringify(Data));
+    console.log(Data);
+});
